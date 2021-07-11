73851763b968023a640e37e96aceee459a0e6924
Ashul <anshulupsc@gmail.com>
2021-07-11 22:15:12 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,9 @@
 const user = require('./user_routes');
 const contact_us = require('./somya-contact');
+const enquiry_long = require('./enquiry-long');
 
 module.exports = function(app){
     app.use('/api/user', user)
     app.use('/api/contact_us/', contact_us)
+    app.use('/api/somya/lenquiry/', enquiry_long)
 }

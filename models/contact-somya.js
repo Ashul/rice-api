843249ef96a78a31c4e0f41db1304ab8995ab6fe
Ashul <anshulupsc@gmail.com>
2021-07-11 22:03:22 +0530

    Create contact-somya.js

---
+++ models/contact-somya.js
@@ -0,0 +1,13 @@
+const mongoose = require ('mongoose');
+const Schema = mongoose.Schema;
+
+const contactSomyaSchema = new Schema({
+    name:{type:String},
+    email:{type:String, trim:true, lowercase:true},
+    subject: { type: String },
+    message: { type: String},
+    created:{type:Date, default:Date.now}
+})
+
+
+module.exports = mongoose.model('ContactSomya', contactSomyaSchema)

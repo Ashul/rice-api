b42c88c8215cbfc5bf22bf959a9e85433311623c
Ashul <anshulupsc@gmail.com>
2021-07-25 11:06:57 +0530

    Create rice-lan-enq.js

---
+++ models/rice-lan-enq.js
@@ -0,0 +1,22 @@
+const mongoose = require ('mongoose');
+const Schema = mongoose.Schema;
+
+const RiceLandEnq = new Schema({
+   
+    fname:{type:String},
+    lname:{type:String},
+    phone:{type:String},
+    email:{type:String},
+
+    town :{type:String},
+    cstate :{type:String},
+    czip:{type:String},
+    cname :{type:String},
+    order_note :{type:String}
+
+
+    created:{type:Date, default:Date.now}
+})
+
+
+module.exports = mongoose.model('RiceLnEnquery', riceLndEnq)

7cc754eb1930f1270160020594f44da28e64c2d7
Ashul <anshulupsc@gmail.com>
2021-08-01 22:44:56 +0530

    Update enquiry-long.js

--- routes/enquiry-long.js
+++ routes/enquiry-long.js
@@ -4,13 +4,26 @@ const router=express.Router()
 const queryLong = require('../models/enquiry-long')
 
 router.post('/somya-enquery-order', (req, res)=>{
-    let orderbill = new queryLong(req.body)    //save User
-    orderbill.save((err,user) => {        // user.hash = undefined;
-        if(err && !user){
-            res.status(401).json({ message:err });
-        }
-    else{ res.status(200).json({ status: 'SUCCESS', data: user })}
-    })
+//     let orderbill = new queryLong(req.body)    //save User
+//     orderbill.save((err,user) => {        // user.hash = undefined;
+//         if(err && !user){
+//             res.status(401).json({ message:err });
+//         }
+//     else{ res.status(200).json({ status: 'SUCCESS', data: user })}
+//     })
+     let querydata = new queryLong(req.body) 
+        queryLong.findOne({mobile:querydata.mobile},function(err,QueryLong1){
+        if(QueryLong1) return res.status(400).json({ auth : false, message :"Mobile Number exits"});
+        querydata.save((err,doc)=>{
+            if(err) {console.log(err);  
+                return res.status(400).json({ success : false});}
+            res.status(200).json({
+                succes:true,
+                user : doc
+            });
+        });
+    });
+    
 }) // end
 
 // all 

4d0be06f69b93062a7c0e27ce943ebf02f2725b8
Ashul <anshulupsc@gmail.com>
2021-08-22 21:54:09 +0530

    Update enquiry-long.js

--- routes/enquiry-long.js
+++ routes/enquiry-long.js
@@ -2,6 +2,20 @@
 const express=require('express') 
 const router=express.Router() 
 const queryLong = require('../models/enquiry-long')
+const nodemailer = require("nodemailer");
+
+// create transporter object with smtp server details
+var transporter = nodemailer.createTransport({
+    host:'smtp.gmail.com',
+    port:587,
+    secure:false,
+    requireTLS:true,
+    //service: 'gmail',
+    auth: {
+      user: 'niraj91k@gmail.com',
+      pass: 'niraj@2022'
+    }
+});
 
 router.post('/somya-enquery-order', (req, res)=>{
 //     let orderbill = new queryLong(req.body)    //save User
@@ -15,6 +29,22 @@ router.post('/somya-enquery-order', (req, res)=>{
         queryLong.findOne({mobile:querydata.mobile},function(err,QueryLong1){
         if(QueryLong1) return res.status(400).json({ auth : false, message :"Mobile Number exits"});
         querydata.save((err,doc)=>{
+	/*--------------------------    */
+            var mailOptions = {
+                from: 'niraj91k@gmail.com',
+                to: 'shyamraj2906@gmail.com',
+                subject: 'Sending Email using Node.js',
+                text: `Full Name: ${doc.name} \n Email: ${doc.email}\n Mobile: ${doc.mobile}   \n Invest Amount: ${doc.f_name} \n Postal/Zip Code: ${doc.postcode} \n Town/City: ${doc.city} \n Franchise Type: ${doc.franchise_type}  \n  State: ${doc.state}  \n SQFT Area: ${doc.sqft_area}  \n Address:${doc.address}  \n  \n Order Note: ${doc.message} \n User ID: ${doc.user_name} \n Password: ${doc.user_pass} `  ,
+              };
+            transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                  console.log(error);
+                } else {
+                  console.log('Email sent: ' + info.response);
+                }
+              });
+      /*--------------------------    */
+		
             if(err) {console.log(err);  
                 return res.status(400).json({ success : false});}
             res.status(200).json({

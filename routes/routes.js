fea9c7871abdfac1e6de36c1084af6a55b0e45e3
Ashul <anshulupsc@gmail.com>
2021-09-21 00:11:16 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const user = require('./user_routes');
 const contact_us = require('./somya-contact');
 const enquiry_long = require('./enquiry-long');
 const ricelandenq = require('./rice-lan-enq');
-//const fortune_enq = require('./fortune_enq');
+const fortune_enq = require('./fortune_enq');
 module.exports = function(app){
     app.use('/api/user', user)
     app.use('/api/somya/contact_us/', contact_us)

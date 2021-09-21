071180a0def1ef752f6ba7b536462860c0da2359
Ashul <anshulupsc@gmail.com>
2021-09-21 23:45:08 +0530

    Update forune_enquiry.js

--- models/forune_enquiry.js
+++ models/forune_enquiry.js
@@ -4,23 +4,16 @@ const Schema = mongoose.Schema;
 const EnqueryLongSchema = new Schema({
    
     name:{type:String},
-    f_name:{type:String},
+    company_name:{type:String},
     email:{type:String},
     mobile:{type:String},
-
-    franchise_type :{type:String},
-    sqft_area :{type:String},
-    //disctrict:{type:String},
+  
     state :{type:String},
     city :{type:String},
     address :{type:String},
     postcode:{type:String},
-    
-    message:{type:String},
-   
-    user_name :{type:String},
-    user_pass :{type:String},
 
+    order_note:{type:String},
     created:{type:Date, default:Date.now}
 })
 
